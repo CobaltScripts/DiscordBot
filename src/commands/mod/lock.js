f5dff39d69655aec88c8d16a389b5abd51db623f
EpsilonPhoenix <129551402+WaterPheonix196@users.noreply.github.com>
2025-10-20 15:15:36 -0400

    fix dumb

--- src/commands/mod/lock.js
+++ src/commands/mod/lock.js
@@ -1,5 +1,5 @@
 import { PermissionFlagsBits } from 'discord.js';
-import { successEmbed } from '../../utils/embeds.js';
+import { successEmbed, errorEmbed } from '../../utils/embeds.js'; 
 
 /** Thank you oblongboot for this superb logic. */
 export default {
@@ -10,48 +10,32 @@ export default {
    * @param {import("discord.js").Message} message
    */
   async execute(client, message) {
-    if (
-      !message.member.permissions.has(PermissionFlagsBits.ManageChannels)
-    ) {
-      return;
+    if (!message.member.permissions.has(PermissionFlagsBits.ManageChannels)) {
+      return message.reply({
+        embeds: [errorEmbed("You don't have permission to manage channels.")],
+        ephemeral: true
+      });
     }
 
     const channel = message.channel;
+    const everyoneRole = message.guild.roles.everyone;
 
     try {
-      const currentOverwrites = channel.permissionOverwrites.cache;
-      let isLocked = true;
+      // check if its already locked
+      const isLocked = channel.permissionOverwrites.cache
+        .get(everyoneRole.id)
+        ?.deny.has(PermissionFlagsBits.SendMessages);
 
-      for (const overwrite of currentOverwrites.values()) {
-        if (
-          overwrite.type === 'role' &&
-          overwrite.deny.has(PermissionFlagsBits.SendMessages)
-        ) {
-          isLocked = true;
-          break;
-        } else {
-          isLocked = false;
-        }
-      }
-
-      for (const [id, role] of message.guild.roles.cache) {
-        if (role.permissions.has(PermissionFlagsBits.Administrator)) continue;
-
-        if (isLocked) {
-          await channel.permissionOverwrites.edit(role, {
-            SendMessages: null
-          });
-        } else {
-          await channel.permissionOverwrites.edit(role, {
-            SendMessages: false
-          });
-        }
-      }
-
+      // instead of sending an API req for every single role, send 1 lmfao
+      await channel.permissionOverwrites.edit(everyoneRole, {
+        SendMessages: isLocked ? null : false,
+      });
+      
       await channel.send({
         embeds: [successEmbed(`**Channel ${isLocked ? 'unlocked' : 'locked'}!**`)],
       });
     } catch (err) {
+      console.error(err);
       await message.reply({
         embeds: [errorEmbed("Something went wrong while toggling the lock.")],
       });
